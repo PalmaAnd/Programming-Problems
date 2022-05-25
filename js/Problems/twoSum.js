6c1ec84054b9a29f7c7fc69e3c743911074506df
PalmaAnd <andrepalma@outlook.de>
2022-05-26 00:07:24 +0200

    Added the same function but with lower memory usage

--- js/Problems/twoSum.js
+++ js/Problems/twoSum.js
@@ -5,28 +5,51 @@
  * You can return the answer in any order.
  *
  * Related topics: Array, Hash Table
- * 
+ *
  */
 
 /**
- *  By using a map, we make sure to only iterate the array once. This results in a time complexity of O(n)
- * 
+ * By using a map, we make sure to only iterate the array once.
+ * This results in a time complexity of O(n).
+ * Runtime: 78 ms
+ * Memory Usage: 43.1 MB
+ *
  * @param {[int]} numbers Array of integers
  * @param {int} target The wished return value
  * @returns Indices of the two numbers such that they add up to target
  */
- var twoSum = function(numbers, target) {
+var twoSum = function (numbers, target) {
     const map = new Map();
-    for(var i = 0; i < numbers.length; i++){
+    for (var i = 0; i < numbers.length; i++) {
         // Calculate what value would be needed to get the target by also using the current value
         const result = target - numbers[i];
         // If the result is already set inside the map, we can return the index of the two values as it will result in the target
-        if(map.has(result)) {
-           return [map.get(result), i];
-         }
+        if (map.has(result)) {
+            return [map.get(result), i];
+        }
         // if the result isn't already in the map we save it
         map.set(numbers[i], i);
     }
 };
 
-module.exports = { twoSum };
+/**
+ *
+ * This function will take longer then twoSum, but only has a Memory Usage of 41.8 MB.
+ * Runtime: 169 ms
+ *
+ * @param {[int]} numbers Array of integers
+ * @param {int} target The wished return value
+ * @returns Indices of the two numbers such that they add up to target
+ */
+var twoSumLowMemory = function (nums, target) {
+    for (var i = 0; i < nums.length - 1; i++) {
+        var temp = nums[i];
+        for (var j = i + 1; j < nums.length; j++) {
+            if (target - temp == nums[j]) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+module.exports = { twoSum, twoSumLowMemory };
--- js/Tests/twoSum.test.js
+++ js/Tests/twoSum.test.js
@@ -11,3 +11,15 @@ test("Two Sum: Should return 6", () => {
 test("Two Sum: Should return 6", () => {
     expect(twoSum.twoSum([3, 3], 6)).toEqual([0, 1]);
 });
+
+test("Two Sum but with lower Memory Usage: Should return 9", () => {
+    expect(twoSum.twoSumLowMemory([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
+
+test("Two Sum but with lower Memory Usage: Should return 6", () => {
+    expect(twoSum.twoSumLowMemory([3, 2, 4], 6)).toEqual([1, 2]);
+});
+
+test("Two Sum but with lower Memory Usage: Should return 6", () => {
+    expect(twoSum.twoSumLowMemory([3, 3], 6)).toEqual([0, 1]);
+});
