08c7ee4c348dfffd22d3120a420787f1f4a5459b
PalmaAnd <andrepalma@outlook.de>
2022-05-25 23:43:36 +0200

    Improved Runtime, added comments and changed to Map

--- js/Problems/twoSum.js
+++ js/Problems/twoSum.js
@@ -4,21 +4,28 @@
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * You can return the answer in any order.
  *
+ * Related topics: Array, Hash Table
+ * 
  */
+
 /**
- *
+ *  By using a map, we make sure to only iterate the array once. This results in a time complexity of O(n)
+ * 
  * @param {[int]} numbers Array of integers
  * @param {int} target The wished return value
  * @returns Indices of the two numbers such that they add up to target
  */
-var twoSum = (numbers, target) => {
-    for(var i = 0; i < numbers.length -1; i++){
-        var temp = numbers[i];
-        for(var j = i+1; j < numbers.length; j++){
-            if (target - temp == numbers[j]){
-                return [i, j];
-            }
-        }
+ var twoSum = function(numbers, target) {
+    const map = new Map();
+    for(var i = 0; i < numbers.length; i++){
+        // Calculate what value would be needed to get the target by also using the current value
+        const result = target - numbers[i];
+        // If the result is already set inside the map, we can return the index of the two values as it will result in the target
+        if(map.has(result)) {
+           return [map.get(result), i];
+         }
+        // if the result isn't already in the map we save it
+        map.set(numbers[i], i);
     }
 };
 
--- js/Tests/twoSum.test.js
+++ js/Tests/twoSum.test.js
@@ -1,13 +1,13 @@
 const twoSum = require('../Problems/twoSum');
 
-test("Two Sum Testing: Returns 9", () => {
+test("Two Sum: Should return 9", () => {
     expect(twoSum.twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
 });
 
-test("Two Sum Testing: Returns 6", () => {
+test("Two Sum: Should return 6", () => {
     expect(twoSum.twoSum([3, 2, 4], 6)).toEqual([1, 2]);
 });
 
-test("Two Sum Testing: Returns 6", () => {
+test("Two Sum: Should return 6", () => {
     expect(twoSum.twoSum([3, 3], 6)).toEqual([0, 1]);
 });
