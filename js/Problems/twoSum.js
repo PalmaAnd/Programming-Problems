11fad97e089ed9ca1efa44f53de83ff64e5ad6bf
PalmaAnd <andrepalma@outlook.de>
2022-05-25 22:30:24 +0200

    Added testcases and first solution for twoSum

--- js/Problems/twoSum.js
+++ js/Problems/twoSum.js
@@ -1,9 +1,24 @@
 /**
- * 
+ * Problem:
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ */
+/**
+ *
  * @param {[int]} numbers Array of integers
  * @param {int} target The wished return value
  * @returns Indices of the two numbers such that they add up to target
  */
-function twoSum(numbers, target) {
-    
-}
+var twoSum = (numbers, target) => {
+    for (var i = 0; i < numbers.length - 1; i++) {
+        for (var j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+module.exports = { twoSum };
--- js/Tests/twoSum.test.js
+++ js/Tests/twoSum.test.js
@@ -1,13 +1,13 @@
-const twoSum = require("../../Problems/twoSum");
+const twoSum = require('../Problems/twoSum');
 
 test("Two Sum Testing: Returns 9", () => {
-    expect(twoSum([2, 7, 11, 15], 9)).toBe([0, 1]);
+    expect(twoSum.twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
 });
 
 test("Two Sum Testing: Returns 6", () => {
-    expect(twoSum([3, 2, 4], 6)).toBe([1, 2]);
+    expect(twoSum.twoSum([3, 2, 4], 6)).toEqual([1, 2]);
 });
 
 test("Two Sum Testing: Returns 6", () => {
-    expect(twoSum([3, 3], 6)).toBe([0, 1]);
+    expect(twoSum.twoSum([3, 3], 6)).toEqual([0, 1]);
 });
