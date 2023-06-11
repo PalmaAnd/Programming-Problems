8bf737b473ce660447cc4ded1353f7338fe17f3e
André Palma <andrepalma@outlook.de>
2023-06-11 22:22:47 +0200

    Finished isAnagram

--- js/Problems/isAnagram.js
+++ js/Problems/isAnagram.js
@@ -5,14 +5,40 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+var isAnagram1 = function(s, t) {
     if (s.length != t.length)
         return false;
-    console.log(s.split('').sort().toString());
-    console.log(t.split('').sort().toString());
     return s.split('').sort().toString() == t.split('').sort().toString();
 };
 
-console.log(isAnagram("rat", "tar"));
-console.log(isAnagram("rat", "car"));
-console.log(isAnagram("aacc", "ccac"));
+/**
+ * After some time I started thinking, if I can use the Unicode value of the characters.
+ * My first approach was wrong, because I only added together all values from s and subtracted the values from t, but
+ * that approach will return true for "ac" and "bb", because they have the same value when added together.
+ * 
+ * But the I thought of using the value of the characters as the index for a counter array which has the length of 26 (because there are 26 letters in the alphabet).
+ * With the help of using charCodeAt and then subtracting 97 (which is the value for a) I was able to fill in the array and then check if all entries are still 0 after going thru
+ * both string.
+ * 
+ * By using this solution I got way better results (in the higher 90%)
+ * 
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    const VALUE_OF_FIRST_LOWERCASE_LETTER = 97;
+    if (s.length != t.length)
+        return false;
+
+    let counter = new Array(26).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        counter[s.charCodeAt(i) - VALUE_OF_FIRST_LOWERCASE_LETTER]++;
+        counter[t.charCodeAt(i) - VALUE_OF_FIRST_LOWERCASE_LETTER]--;
+    }
+    for (let i = 0; i < counter.length - 1; i++) {
+        if (counter[i] != 0) return false;
+    }
+
+    return true;
+}
