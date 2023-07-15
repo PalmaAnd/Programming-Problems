e7758366fcde4e26f79b51ef369f7a82e13d70ff
PalmaAnd <andrepalma@outlook.de>
2023-07-15 15:36:58 +0200

    Started working on Group Anagrams (https://leetcode.com/problems/group-anagrams/)

---
+++ Java/src/Group_Anagrams.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * <p>
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ */
+public class Group_Anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> solution = new ArrayList<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] temp = strs[i].toCharArray();
+
+        }
+        return solution;
+    }
+}
