8dc72c39cd5b1bb6d63d3ed4ddb79da152c7e911
PalmaAnd <andrepalma@outlook.de>
2023-08-22 21:48:26 +0200

    Started working on new problem

--- js/Problems/isPalindrome.js
+++ js/Problems/isPalindrome.js
@@ -3,5 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    
+    s = s.trim();
+    for (let index = 0; index < s.length; index++) {
+        const element = s[index];
+        if (s.charAt(index).toLocaleLowerCase() != s.charAt(s.length - index).toLocaleLowerCase()){
+            return false;
+        }
+    }
+    return true;
 };
+
+module.exports = isPalindrome;
---
+++ js/Tests/isPalindrome.test.js
@@ -0,0 +1,13 @@
+const isPalindrome = require('../Problems/isPalindrome');
+
+test("TestPalindrom: true", () => {
+    expect(isPalindrome("A man, a plan, a canal: Panama")).toBe(true);
+});
+
+test("TestPalindrom: false", () => {
+    expect(isPalindrome("race a car")).toBe(false);
+});
+
+test("TestPalindrom: empty string", () => {
+    expect(isPalindrome(" ")).toBe(true);
+});
