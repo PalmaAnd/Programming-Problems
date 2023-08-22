464dbeb780f831681fb60a813295500ab6d9816d
PalmaAnd <andrepalma@outlook.de>
2023-08-22 22:00:41 +0200

    Solved isPalindrome issue

--- js/Problems/isPalindrome.js
+++ js/Problems/isPalindrome.js
@@ -1,12 +1,15 @@
 /**
- * @param {string} s
- * @return {boolean}
+ * Check if a string is a palindrome after removing non-alphanumeric characters and ignoring case.
+ * @param {string} s - The input string to check.
+ * @return {boolean} - True if the input string is a palindrome, false otherwise.
  */
 var isPalindrome = function(s) {
-    s = s.trim();
+    // Remove non-alphanumeric characters and convert the string to lowercase
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
     for (let index = 0; index < s.length; index++) {
-        const element = s[index];
-        if (s.charAt(index).toLocaleLowerCase() != s.charAt(s.length - index).toLocaleLowerCase()){
+        // Compare chars at the start and the end of the string
+        if (s.charAt(index) !== s.charAt(s.length - index - 1)){
             return false;
         }
     }
