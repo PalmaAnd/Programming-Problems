8a403758b238d234815b411e94927203efd0b052
PalmaAnd <andrepalma@outlook.de>
2023-09-05 20:51:45 +0200

    Started working on MinStack

--- Java/src/main/java/problems/leetcode/stack/MinStack.java
+++ Java/src/main/java/problems/leetcode/stack/MinStack.java
@@ -1,4 +1,40 @@
 package problems.leetcode.stack;
 
-public class MinStack {
+import java.util.Stack;
+
+class MinStack {
+
+    private int min;
+    private Stack stack;
+
+    public MinStack() {
+        min = Integer.MAX_VALUE;
+        stack = new Stack<Integer>();
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        int topValue = (int) stack.get(stack.size());
+        return topValue;
+    }
+
+    public int getMin() {
+        return min;
+    }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
