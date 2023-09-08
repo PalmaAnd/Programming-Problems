cfcf1475c759315a89b67cdb5d15e51dcd744571
PalmaAnd <andrepalma@outlook.de>
2023-09-08 23:21:24 +0200

    Added stack solution for Daily Temps

--- Java/src/main/java/problems/leetcode/stack/DailyTemperatures.java
+++ Java/src/main/java/problems/leetcode/stack/DailyTemperatures.java
@@ -4,10 +4,21 @@ import java.util.Stack;
 
 public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
         int[] answer = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < temperatures.length; i++) {
+            // While the stack is not empty and the current temperature is greater than the temperature at the index
+            // stored at the top of the stack, we have found a warmer day.
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                // Get the index of the temperature for which we found a warmer day.
+                int prevIndex = stack.pop();
+
+                // Calculate the number of days waited and store it in the answer array.
+                answer[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
         }
+
         return answer;
     }
 
