32e5d53b6a7475b56cb310931f63305d2d36767f
André Palma <54452893+PalmaAnd@users.noreply.github.com>
2023-09-09 17:38:44 +0200

    Finally found a working solution for ProductExceptSelf

--- Java/src/main/java/problems/leetcode/arrays_hashing/ProductExceptSelf.java
+++ Java/src/main/java/problems/leetcode/arrays_hashing/ProductExceptSelf.java
@@ -1,7 +1,25 @@
 package problems.leetcode.arrays_hashing;
 
 public class ProductExceptSelf {
+
     public int[] productExceptSelf(int[] nums) {
+        int arrayLength = nums.length;
+        int[] answer = new int[arrayLength];
+        int[] prefix = new int[arrayLength];
+        int[] suffix = new int[arrayLength];
+        prefix[0] = 1; // The first element does not have a prefix, so we set it neutral (1 for multiplications)
+        suffix[arrayLength-1] = 1; // Same goes for the last element as it does not have a suffix
+        for (int i = 1; i < arrayLength; i++) {
+            prefix[i] = nums[i - 1] * prefix[i - 1];
+            suffix[arrayLength - i - 1] = nums[arrayLength - i] * suffix[arrayLength - i];
+        }
+        for (int i = 0; i < arrayLength; i++) {
+            answer[i] = prefix[i] * suffix[i];
+        }
+        return answer;
+    }
+
+    public int[] productExceptSelfBruteForce(int[] nums) {
         int[] answer = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             answer[i] = 1;
--- Java/src/test/java/problems/leetcode/arrays_hashing/ProductExceptSelfTest.java
+++ Java/src/test/java/problems/leetcode/arrays_hashing/ProductExceptSelfTest.java
@@ -7,7 +7,9 @@ public class ProductExceptSelfTest {
         ProductExceptSelf productObject = new ProductExceptSelf();
         int[] input = {1,2,3,4};
         System.out.println(Arrays.toString(productObject.productExceptSelf(input)));
+        System.out.println(Arrays.toString(productObject.productExceptSelfBruteForce(input)));
         int[] input2 =  {-1,1,0,-3,3};
         System.out.println(Arrays.toString(productObject.productExceptSelf(input2)));
+        System.out.println(Arrays.toString(productObject.productExceptSelfBruteForce(input2)));
     }
 }
