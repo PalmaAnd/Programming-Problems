ab1daf45ca4dd9a584e44c20b379856b15901d30
André Palma <54452893+PalmaAnd@users.noreply.github.com>
2023-09-10 10:03:39 +0200

    Started working on 3sum problem

---
+++ Java/src/main/java/problems/leetcode/two_pointers/ThreeSum.java
@@ -0,0 +1,17 @@
+package problems.leetcode.two_pointers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        ArrayList<Integer> helperList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int currentVal = nums[i];
+            while (helperList.size() < 3){
+            }
+        }
+        return answer;
+    }
+}
