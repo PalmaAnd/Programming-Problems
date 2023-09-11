9e2c72b68ad1838bfc35448e092ffea3b12bdc6e
PalmaAnd <andrepalma@outlook.de>
2023-09-11 23:02:24 +0200

    Continued work on 3sum

--- Java/src/main/java/problems/leetcode/two_pointers/ThreeSum.java
+++ Java/src/main/java/problems/leetcode/two_pointers/ThreeSum.java
@@ -6,12 +6,28 @@ import java.util.List;
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        ArrayList<Integer> helperList = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int currentVal = nums[i];
-            while (helperList.size() < 3){
-            }
+        ArrayList<Integer> sortedList = sortList(nums);
+        for (int i = 0; i < sortedList.size(); i++){
+
         }
         return answer;
+    }
+
+    public ArrayList<Integer> sortList(int[] list){
+        ArrayList<Integer> sortedList = new ArrayList<>();
+        sortedList.add(list[0]);
+        // Create and fill sorted list
+        for (int i = 1; i < list.length - 2; i++) {
+            int j = 0;
+            int currentVal = list[i];
+            while(j < sortedList.size() && currentVal < sortedList.get(j)){
+                if (currentVal < sortedList.get(j)){
+                    sortedList.add(j, currentVal);
+                    break;
+                }
+                j++;
+            }
+        }
+        return sortedList;
     }
 }
