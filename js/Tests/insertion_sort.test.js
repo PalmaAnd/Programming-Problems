734962672a070b81037bd88d1cab5e46585f8341
PalmaAnd <andrepalma@outlook.de>
2023-09-12 00:24:53 +0200

    Removed js insertion sort attempt to fix failing pipeline

--- js/Tests/insertion_sort.test.js
+++
@@ -1,5 +0,0 @@
-const insertion_sort = require('../Problems/insertion_sort.js');
-
-test("Insertion Sort: ", () => {
-    expect(insertion_sort.insertionSortList([4,2,1,3])).toEqual([1,2,3,4]);
-});
