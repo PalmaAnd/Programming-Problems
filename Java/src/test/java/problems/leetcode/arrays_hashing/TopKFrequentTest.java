e05b7bd59508a1a97374342daaed7003c8dae0bf
PalmaAnd <andrepalma@outlook.de>
2023-09-13 23:09:07 +0200

    Added the rest of the tests

--- Java/src/test/java/problems/leetcode/arrays_hashing/TopKFrequentTest.java
+++ Java/src/test/java/problems/leetcode/arrays_hashing/TopKFrequentTest.java
@@ -1,5 +1,28 @@
 package problems.leetcode.arrays_hashing;
 
-public class TopKFrequentTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class TopKFrequentTest {
+
+    @Test
+    void testSampleInput() {
+        TopKFrequent topKFrequent = new TopKFrequent();
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] expected = {1, 2};
+        int[] result = topKFrequent.topKFrequent(nums, k);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void testEmptyInput() {
+        TopKFrequent topKFrequent = new TopKFrequent();
+        int[] nums = {};
+        int k = 0;
+        int[] expected = {};
+        int[] result = topKFrequent.topKFrequent(nums, k);
+        assertArrayEquals(expected, result);
+    }
 }
