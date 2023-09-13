4441fc97f056b44ea29323e291ee3b50b9edadbf
PalmaAnd <andrepalma@outlook.de>
2023-09-13 22:37:30 +0200

    Added maven testing: junit.jupiter Started working on the testfiles

--- Java/src/test/java/problems/leetcode/utils/ListNodeTest.java
+++ Java/src/test/java/problems/leetcode/utils/ListNodeTest.java
@@ -1,7 +1,7 @@
-package problems.leetcode.linked_list;
+package problems.leetcode.utils;
 
 import org.junit.jupiter.api.Test;
-import problems.leetcode.utils.ListNode;
+import problems.leetcode.linked_list.ReverseLinkedList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,11 +21,10 @@ class ReverseLinkedListTest {
         // Reverse the list
         ListNode revHead = revList.reverseList(head);
 
-        // Assert that the reversed list is as expected: 5 -> 4 -> 3 -> 2 -> 1
-        assertEquals(5, revHead.val);
-        assertEquals(4, revHead.next.val);
-        assertEquals(3, revHead.next.next.val);
-        assertEquals(2, revHead.next.next.next.val);
-        assertEquals(1, revHead.next.next.next.next.val);
+        // Convert the reversed list to a string for testing
+        String reversedListString = revHead.toString();
+
+        // Assert that the reversed list matches the expected result
+        assertEquals("5 -> 4 -> 3 -> 2 -> 1", reversedListString);
     }
 }
