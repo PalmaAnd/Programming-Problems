ff04021f468e7982a12ee9f60dc1b53d713be1fd
PalmaAnd <andrepalma@outlook.de>
2023-09-17 22:06:27 +0200

    Started working on new problem

--- Java/src/main/java/problems/leetcode/arrays_hashing/GroupAnagrams.java
+++ Java/src/main/java/problems/leetcode/arrays_hashing/GroupAnagrams.java
@@ -1,9 +1,15 @@
 package problems.leetcode.arrays_hashing;
 
-import java.util.List;
+import java.util.*;
 
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str:strs) {
+            String sortedStr;
+        }
+
         return null;
     }
 }
