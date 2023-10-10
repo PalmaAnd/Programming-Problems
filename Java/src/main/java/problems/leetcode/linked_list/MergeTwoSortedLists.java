f9e4145482a3298122532fc169bbc8390bf871a5
André Palma <54452893+PalmaAnd@users.noreply.github.com>
2023-10-10 15:35:52 +0200

    Found working solution for MergeTwoSortedLists

--- Java/src/main/java/problems/leetcode/linked_list/MergeTwoSortedLists.java
+++ Java/src/main/java/problems/leetcode/linked_list/MergeTwoSortedLists.java
@@ -4,6 +4,19 @@ import problems.leetcode.utils.ListNode;
 
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return null;
+        ListNode sortedList = new ListNode();
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+        if (list1.val < list2.val){
+            sortedList = list1;
+            list1 = list1.next;
+        } else {
+            sortedList = list2;
+            list2 = list2.next;
+        }
+        sortedList.next = mergeTwoLists(list1, list2);
+        return sortedList;
     }
 }
