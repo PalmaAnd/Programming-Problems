3c7972b54abfb2dd41f49ef341e30f108d4e7465
André Palma <54452893+PalmaAnd@users.noreply.github.com>
2023-11-06 11:23:52 +0100

    Reset the problem to start working at it with a fresh mind

--- Java/src/main/java/problems/leetcode/arrays_hashing/MergeSortedArrays.java
+++ Java/src/main/java/problems/leetcode/arrays_hashing/MergeSortedArrays.java
@@ -10,23 +10,6 @@ public class MergeSortedArrays {
      * and should be ignored. nums2 has a length of n.
      */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] < nums2[j]){
-                    moveArray(nums1, i, m);
-                    nums1[i] = nums2[j];
-                }
-            }
-        }
-    }
 
-    private void moveArray(int[] nums1, int i, int size){
-        int val = nums1[i];
-        while (i < size - 1){
-            int help = nums1[i + 1];
-            nums1[i + 1] = val;
-            val = help;
-            i++;
-        }
     }
 }
